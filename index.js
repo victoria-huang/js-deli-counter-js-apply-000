866076b4c142cb91c09ab95e4d3b2e55cd249ecc
Victoria Huang <vctrah@gmail.com>
2018-01-09 01:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   var placeInLine = katzDeliLine.indexOf(name) + 1;
-  return placeInLine;
+  return "Welcome, " + name + ". You are number " + placeInLine + " in line.";
 }
+
+function nowServing
